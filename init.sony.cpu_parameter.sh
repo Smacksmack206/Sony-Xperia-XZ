ea3895e63c1cff2a9bf5dc09b874ee7d04e7c768
Smacksmack206 <cvallieu94@gmail.com>
2017-05-28 21:45:44 -0700

    Add files via upload
    Removed Sony stock input boost completely with use of all three files: Deeznuts/init.qcom.post_boot.sh/init.sony.cpu_parameter.sh

---
+++ init.sony.cpu_parameter.sh
@@ -0,0 +1,11 @@
+#!/system/bin/sh
+# *********************************************************************
+# * Copyright 2016 (C) Sony Mobile Communications Inc.                *
+# * All rights, including trade secret rights, reserved.              *
+# *********************************************************************
+#
+
+# set cpu_boost parameters
+echo "0" > /sys/module/cpu_boost/parameters/input_boost_ms
+
+exit 0
